63db0f67a586bad6290cc5055389ceab08719f1d
sachiyaofifical943-debug <sachiyaofifical943@gmail.com>
2025-11-27 09:11:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachiyaofifical943-debug/S-A-C-H-I_B-O-T/blob/main/Images/1000020198.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 SACHI-MD Is Alive Now😍*",
+BOT_OWNER: '94726584941',  // Replace with the owner's phone number
 
 
 
